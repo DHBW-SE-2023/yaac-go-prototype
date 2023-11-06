61639171ac4809b39bf083c16903573e8d56ad2b
BloodLaad <maxalberti13@gmail.com>
2023-11-06 17:54:32 +0100

    Initial app based on the fyne tutorial. With tests

--- main.go
+++ main.go
@@ -1,14 +1,64 @@
 package main
 
 import (
-	"fyne.io/fyne/app"
-	"fyne.io/fyne/widget"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
+
+func updateTime(clock *widget.Label) {
+	formatted := time.Now().Format("Current system time: 15:04:05")
+	clock.SetText(formatted)
+}
+
+func makeUI_w1() *widget.Label {
+	clock := widget.NewLabel("")
+	updateTime(clock)
+	return clock
+}
+
+func makeUI_w2(a fyne.App) *widget.Button {
+	return widget.NewButton("Open new", func() {
+		w3 := a.NewWindow("Third")
+		w3.SetContent(container.NewVBox(makeUI_w3()))
+		w3.Resize(fyne.NewSize(200, 50))
+		w3.Show()
+	})
+}
+
+func makeUI_w3() (*widget.Label, *widget.Entry) {
+	out_label := widget.NewLabel("Hello World!")
+	in_entry := widget.NewEntry()
+
+	in_entry.OnChanged = func(s string) {
+		out_label.SetText(s + " to you as well!")
+	}
+
+	return out_label, in_entry
+}
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("System time")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
-	w.ShowAndRun()
+	clock := makeUI_w1()
+	w.SetContent(clock)
+	// Update time in anonymous go-routine
+	go func() {
+		for range time.Tick(time.Second) {
+			updateTime(clock)
+		}
+	}()
+	w.SetMaster()
+	w.Show()
+
+	w2 := a.NewWindow("Larger")
+	w2.SetContent(makeUI_w2(a))
+	w2.Resize(fyne.NewSize(250, 100))
+	w2.Show()
+
+	a.Run()
 }
---
+++ main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+
+	"fyne.io/fyne/v2/test"
+)
+
+func TestThirdWindowUI(t *testing.T) {
+	out_label, in_entry := makeUI_w3()
+
+	// initial state
+	if out_label.Text != "Hello World!" {
+		t.Error("Incorrect initial value")
+	}
+
+	// user input
+	test.Type(in_entry, "Hi")
+	if out_label.Text != "Hi to you as well!" {
+		t.Error("Incorrect user input handling")
+	}
+}
