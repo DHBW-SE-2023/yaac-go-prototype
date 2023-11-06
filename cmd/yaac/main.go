ddf9b4723054bfdb87cb4f655aca273b531ad637
BloodLaad <maxalberti13@gmail.com>
2023-11-07 00:12:29 +0100

    Adjusted main for prototype goals

--- cmd/yaac/main.go
+++ cmd/yaac/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"time"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/widget"
 	yaac_io "github.com/DHBW-SE-2023/yaac-go-prototype/pkg"
@@ -42,44 +39,11 @@ func main() {
 	// handle main window
 	mainWindow.SetContent(makeUI_w1())
 	mainWindow.Show()
-	mainWindow.SetMaster()
-
-	// handle window 2
-	w2 := App.NewWindow("Larger")
-	w2.SetContent(makeUI_w2())
-	w2.Resize(fyne.NewSize(250, 100))
-	w2.Show()
 
 	App.Run()
 }
 
-func updateTime(clock *widget.Label) {
-	formatted := time.Now().Format("Current system time: 15:04:05")
-	clock.SetText(formatted)
-}
-
 func makeUI_w1() *widget.Label {
-	clock := widget.NewLabel("")
-	updateTime(clock)
+	clock := widget.NewLabel("Hi!")
 	return clock
-}
-
-func makeUI_w2() *widget.Button {
-	return widget.NewButton("Open new", func() {
-		w3 := App.NewWindow("Third")
-		w3.SetContent(container.NewVBox(makeUI_w3()))
-		w3.Resize(fyne.NewSize(200, 50))
-		w3.Show()
-	})
-}
-
-func makeUI_w3() (*widget.Label, *widget.Entry) {
-	out_label := widget.NewLabel("Hello World!")
-	in_entry := widget.NewEntry()
-
-	in_entry.OnChanged = func(s string) {
-		out_label.SetText(s + " to you as well!")
-	}
-
-	return out_label, in_entry
 }
