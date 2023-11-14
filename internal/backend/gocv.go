0fc00f969a609c348877eb3268c969a92fd61244
MaxAlberti <maxalberti@gmx.de>
2023-11-14 16:47:05 +0100

    First WIP

--- internal/backend/gocv.go
+++ internal/backend/gocv.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"math"
 
 	"gocv.io/x/gocv"
 )
@@ -54,57 +55,121 @@ func biggest_contour(contours gocv.PointsVector, min_area float64) (int, gocv.Po
 	return biggest_n, approx_contour
 }
 
-func order_points(pts gocv.PointVector) gocv.PointVector {
-	// ToDo
-}
+func order_points(points gocv.PointVector) gocv.PointVector {
+	/*
+			initialzie a list of coordinates that will be ordered
+		    such that the first entry in the list is the top-left,
+		    the second entry is the top-right, the third is the
+		    bottom-right, and the fourth is the bottom-left
+	*/
 
-// ## Find the exact (x,y) coordinates of the biggest contour and crop it out
-func four_point_transform(image gocv.Mat, pts gocv.PointVector) gocv.Mat {
-	// obtain a consistent order of the points and unpack them
-    // individually
-    rect := order_points(pts)
-    (tl, tr, br, bl) := rect
+	pts := points.ToPoints()
+	rect := []image.Point{
+		image.Point{0, 0},
+		image.Point{0, 0},
+		image.Point{0, 0},
+		image.Point{0, 0},
+	}
+
+	// Get Rectangle Points
+	arg_min_sum := math.MaxInt
+	arg_max_sum := math.MinInt
+	arg_min_dif := math.MaxInt
+	arg_max_dif := math.MinInt
+	for i := 0; i < points.Size(); i++ {
+		pnt := points.At(i)
+
+		// the top-left point will have the smallest sum, whereas
+		// the bottom-right point will have the largest sum
+		sum := pnt.X + pnt.Y
+		if sum < arg_min_sum {
+			arg_min_sum = sum
+		}
+		if sum > arg_max_sum {
+			arg_max_sum = sum
+		}
+
+		// now, compute the difference between the points, the
+		// top-right point will have the smallest difference,
+		// whereas the bottom-left will have the largest difference
+		dif := pnt.Y - pnt.X
+		if dif < arg_min_dif {
+			arg_min_dif = dif
+		}
+		if dif > arg_max_dif {
+			arg_max_dif = dif
+		}
+	}
+	rect[0] = pts[arg_min_sum]
+	rect[2] = pts[arg_max_sum]
+	rect[1] = pts[arg_min_dif]
+	rect[3] = pts[arg_max_dif]
+
+	// return the ordered coordinates
+	return gocv.NewPointVectorFromPoints(rect)
 }
 
 /*
-def four_point_transform(image, pts):
-    # obtain a consistent order of the points and unpack them
-    # individually
-    rect = order_points(pts)
-    (tl, tr, br, bl) = rect
+	# now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
 
-    # compute the width of the new image, which will be the
-    # maximum distance between bottom-right and bottom-left
-    # x-coordiates or the top-right and top-left x-coordinates
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
+    # return the ordered coordinates
+    return rect
+*/
 
-    # compute the height of the new image, which will be the
-    # maximum distance between the top-right and bottom-right
-    # y-coordinates or the top-left and bottom-left y-coordinates
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
+// ## Find the exact (x,y) coordinates of the biggest contour and crop it out
+func four_point_transform(img gocv.Mat, pts gocv.PointVector) gocv.Mat {
+	// obtain a consistent order of the points and unpack them
+	// individually
+	rect := order_points(pts)
 
-    # now that we have the dimensions of the new image, construct
-    # the set of destination points to obtain a "birds eye view",
-    # (i.e. top-down view) of the image, again specifying points
-    # in the top-left, top-right, bottom-right, and bottom-left
-    # order
-    dst = np.array([
-        [0, 0],
-        [maxWidth - 1, 0],
-        [maxWidth - 1, maxHeight - 1],
-        [0, maxHeight - 1]], dtype="float32")
+	//(tl, tr, br, bl) := rect @LEANDER
+	rec_arr := rect.ToPoints()
+	tl := rec_arr[0]
+	tr := rec_arr[1]
+	br := rec_arr[2]
+	bl := rec_arr[3]
 
-    # compute the perspective transform matrix and then apply it
-    M = cv2.getPerspectiveTransform(rect, dst)
-    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+	// compute the width of the new image, which will be the
+	// maximum distance between bottom-right and bottom-left
+	// x-coordiates or the top-right and top-left x-coordinates
+	widthA := math.Sqrt(math.Pow(float64(br.X-bl.X), 2) + math.Pow(float64(br.Y-bl.Y), 2))
+	widthB := math.Sqrt(math.Pow(float64(tr.X-tl.X), 2) + math.Pow(float64(tr.Y-tl.Y), 2))
+	maxWidth := max(int(widthA), int(widthB))
 
-    # return the warped image
-    return warped
- */
+	// compute the height of the new image, which will be the
+	// maximum distance between the top-right and bottom-right
+	// y-coordinates or the top-left and bottom-left y-coordinates
+	heightA := math.Sqrt(math.Pow(float64(tr.X-br.X), 2) + math.Pow(float64(tr.Y-br.Y), 2))
+	heightB := math.Sqrt(math.Pow(float64(tl.X-bl.X), 2) + math.Pow(float64(tl.Y-bl.Y), 2))
+	maxHeight := max(int(heightA), int(heightB))
+
+	/*
+		now that we have the dimensions of the new image, construct
+		the set of destination points to obtain a "birds eye view",
+		(i.e. top-down view) of the image, again specifying points
+		in the top-left, top-right, bottom-right, and bottom-left
+		order
+	*/
+	dst := gocv.NewPointVectorFromPoints([]image.Point{
+		image.Point{0, 0},
+		image.Point{maxWidth - 1, 0},
+		image.Point{maxWidth - 1, maxHeight - 1},
+		image.Point{0, maxHeight - 1},
+	})
+
+	// compute the perspective transform matrix and then apply it
+	M := gocv.GetPerspectiveTransform(rect, dst)
+	warped := gocv.NewMat()
+	gocv.WarpPerspective(img, &warped, M, image.Point{maxWidth, maxHeight})
+
+	// return the warped image
+	return warped
+}
 
 /*
 # Transformation the image
