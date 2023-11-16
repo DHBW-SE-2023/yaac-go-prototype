87a5b87728205e46fe273a86214c3ff6d1abab14
Max <73841659+MaxAlberti@users.noreply.github.com>
2023-11-16 10:53:18 +0100

    Update main.go
    Made UI work after merge

--- cmd/yaac/main.go
+++ cmd/yaac/main.go
@@ -4,6 +4,5 @@ import yaac_mvvm "github.com/DHBW-SE-2023/yaac-go-prototype/internal/mvvm"
 
 func main() {
 	mvvm := yaac_mvvm.New()
-	//mvvm.OpenMainWindow()
-	mvvm.StartGoCV()
+	mvvm.OpenMainWindow()
 }
